3ae4decffe2a5a421c92295ab15056db69ea79f9
victoraldir <vhaldir@gmail.com>
2016-09-19 23:40:13 +0100

    Initial comit

---
+++ app/src/main/java/com/quartzo/myappportfolio/MainActivity.java
@@ -0,0 +1,21 @@
+package com.quartzo.myappportfolio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void sendMessage(View view) {
+        Toast.makeText(this,((Button) view).getText().toString(),Toast.LENGTH_SHORT);
+    }
+}
